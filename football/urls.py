8dd18185f32682d5be877ee6a14a3e0eea857ef2
OmonovSarvar <sarvaromon33@gmail.com>
2023-11-22 19:52:32 +0500

    Make changes im football/url.py admin url route change to creator

--- football/urls.py
+++ football/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('creator/', admin.site.urls),
     path('api/', include('football_api.urls')),
     path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
     path('api/schema/swagger/', SpectacularSwaggerView.as_view(url_name='schema')),
